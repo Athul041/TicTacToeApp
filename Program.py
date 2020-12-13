6fa7d0e62bbbf1527ff6a25742f56c2d745e81a2
Athul Chandran <athul041@gmail.com>
2020-12-14 01:02:49 +0530

    basic game logic working

--- Program.py
+++ Program.py
@@ -1,93 +1,87 @@
 import random
 
-def tic_tac_check(arr):
-    ax, ay = arr[0]
-    bx, by = arr[1]
-    cx, cy = arr[2]
-    print("a = ", ax, ay)
-    print("b = ", bx, by)
-    print("c = ", cx, cy)
+def tic_tac_check(a, b, c):
+    ax, ay = a
+    bx, by = b
+    cx, cy = c
     if((ax==bx and bx==cx) or (ay==by and by==cy)):
-        print("1")
         return True
     elif(ax==ay and bx==by and cx==cy):
-        print("2")
         return True
     elif((ax+ay)==2 and (ax+ay)==(bx+by) and (bx+by)==(cx+cy)):
-        print("3")
         return True
     else:
-        print("4")
         return False
 
-def get_O_next(X, O, Unselected):
-    for x in O:
-        for y in O.pop(O.indexof(x)):
-            for row in Unselected:
-                for z in row:
-                    if(tic_tac_check(x,y,z)):
-                        return z
-    for x in X:
-        for y in X.pop(X.indexof(x)):
-            for z in Unselected:
+def get_o_selected_next(x_selected, o_selected, unselected):
+    for x in o_selected:
+        for y in [i for i in o_selected if i != x]:
+            for z in unselected:
                 if(tic_tac_check(x,y,z)):
+                    print(f"z = {z}")
                     return z
-    return random.choice(Unselected[0])
+    for x in x_selected:
+        for y in [i for i in x_selected if i != x]:
+            for z in unselected:
+                if(tic_tac_check(x,y,z)):
+                    print(f"z = {z}")
+                    return z
+    return random.choice(unselected)
 
-def print_board(X,O,Unselected):
+def print_board(x_selected, o_selected, unselected):
     for i in range(3):
             for j in range(3):
-                if(not Unselected[i][j]):
-                    if((i,j) in X):
-                        print("X")
-                    elif((i,j) in O):
-                        print("O")
+                if not ((i,j) in unselected):
+                    if (i,j) in x_selected:
+                        print(" X ", end =" ")
+                    elif (i,j) in o_selected:
+                        print(" O ", end =" ")
                 else:
-                    print(i,j, end =" ")    
+                    print(f"{i},{j}", end =" ")
             print('\n')
 
-def remove_Unselected(arr, Unselected):
-    for row in Unselected:
-        if((x,y) in row):
-            row.remove(arr)
-            return True
-    return False
+def win_check(tArray):
+    for x in tArray:
+        for y in [i for i in tArray if i != x]:
+            for z in [i for i in tArray if (i !=x and i != y)]:
+                if(tic_tac_check(x,y,z)):
+                    return True
 
-def check_in_Unselected(arr, Unselected):
-    print("Checking {0} in Unselected", arr)
-    for row in Unselected:
-        print(row)
-        if((x,y) in row):
-            return True
-    return False
-
-X = []
-O = []
-Unselected = [[(x,y) for x in range(3)] for y in range(3)]
+x_selected = []
+o_selected = []
+unselected = [(x,y) for x in range(3) for y in range(3)]
 
 game_on = 0
 
-print(Unselected)
 print("Press Enter to Start")
 if(not input()):
     game_on = 1
-    while(game_on == 1):
+    while game_on == 1 and len(unselected) > 0:
+        
         print("You are X. Select cell")
-        print_board(X,O,Unselected)
+        print_board(x_selected, o_selected, unselected)
+
         i = input()
         if i:
-            x,y = i.split(',')
-            if(check_in_Unselected((x,y), Unselected)):
-                X.append((x,y))
-                remove_Unselected((x,y), Unselected)
-                print_board(X,O,Unselected)
-                # get_O_next(X,O,Unselected)
-                game_on = 0
-                break
-            else:
-                game_on = 0
-                break
-            inp = input()
-            if inp:
-                game_on = 0
+            if len(i.split(',')) == 2:
+                x, y = [int(i) for i in i.split(',')]
+                if (x, y) in unselected:
+                    print("Unselected")
+                    x_selected.append((x,y))
+                    unselected.remove((x,y))
+                    if win_check(x_selected):
+                        game_on = 0
+                        break
+                    print_board(x_selected, o_selected, unselected)
+                    o_next = get_o_selected_next(x_selected, o_selected, unselected)
+                    o_selected.append(o_next)
+                    unselected.remove(o_next)
+                    if win_check(o_selected):
+                        game_on = 0
+                        break
+                else:
+                    print("Already selected")
+    print("GAME OVER")
+    print_board(x_selected, o_selected, unselected)
+    inp = input()
 
